8b7065230326487677b36b614b569925ae5c8d50
Ophir Bentov <wannabegurux@gmail.com>
2023-08-10 15:37:49 -0700

    completed delete button functionality for product-list.tsx

--- pages/product-list.tsx
+++ pages/product-list.tsx
@@ -1,30 +1,35 @@
 import { useState } from "react";
-import {Table, TableBody, TableCell, TableRow, Button} from '@mui/material';
-import productData from '../data/product-fixtures.json'
+import { Table, TableBody, TableCell, TableRow, Button } from "@mui/material";
+import productData from "../data/product-fixtures.json";
+import Link from "next/link";
 
 const ProductList = () => {
-    const [products, setProducts]= useState(productData);
-    
-    return(
-        <Table>
-          <TableBody>
-            {products.map(product => (
-              <TableRow key={product.id}>
-                <TableCell>{product.name}</TableCell>
-                <TableCell>{product.color}</TableCell>
-                <TableCell>{product.type}</TableCell>
-                <TableCell>{product.price}</TableCell>
-                <TableCell>
-                  <Button>Edit</Button>
-                  <Button>Delete</Button>
-                </TableCell>
-              </TableRow>
+  const [products, setProducts] = useState(productData);
+
+  const handleDelete = (id: string) => {
+    setProducts(products.filter((product) => product.id !== id));
+  };
+
+  return (
+    <Table>
+      <TableBody>
+        {products.map((product) => (
+          <TableRow key={product.id}>
+            <TableCell>{product.name}</TableCell>
+            <TableCell>{product.color}</TableCell>
+            <TableCell>{product.type}</TableCell>
+            <TableCell>{product.price}</TableCell>
+            <TableCell>
+              <Link href={`/product-detail/${product.sku}`}>
+                <Button>Edit</Button>
+              </Link>
+              <Button onClick={() => handleDelete(product.id)}>Delete</Button>
+            </TableCell>
+          </TableRow>
         ))}
-          </TableBody>
-        </Table>
-
-    );
+      </TableBody>
+    </Table>
+  );
 };
-
 
 export default ProductList;
